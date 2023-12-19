8c0b6f28a0dfcc475d53ad1509aeab0c81bee1de
reshail2 <reshail.muzammil@pfizer.com>
2023-12-19 15:42:32 +0500

    Carousel update

--- blocks/carousel/ carousel.js
+++ blocks/carousel/ carousel.js
@@ -1,7 +1,11 @@
 export default function decorate(block) {
-    [...block.children].forEach((row) => {
-        [...row.children].forEach((col) => {
-
-        });
+  [...block.children].forEach((row, i) => {
+    row.classList.add('carouse-item');
+    if (i === 0) {
+      row.classList.add('active');
+    }
+    [...row.children].forEach((col) => {
+      col.children[1].style = 'position: absolute; left: 0; top: 0;';
     });
+  });
 }
